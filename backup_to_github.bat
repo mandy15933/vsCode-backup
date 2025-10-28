187417ba54065526d92cb899987e038c289d8abc
mandy15933 <11361135@nfu.edu.tw>
2025-10-28 16:17:31 +0800

    Auto backup on 2025-10-28_下午04

--- backup_to_github.bat
+++ backup_to_github.bat
@@ -1,9 +1,7 @@
 @echo off
-:: ====== GitHub 自動備份批次檔 ======
-:: 作者：ChatGPT (for mandy15933)
-:: 時間會自動記錄在 commit 訊息中
-
-cd /d "C:\xampp\htdocs"   :: ← 請改成你的專案資料夾路徑
+:: ====== GitHub 自動備份批次檔 (繁體中文版修正版) ======
+:: 將以下路徑改成你的專案資料夾
+cd /d "C:\xampp\htdocs"
 
 :: 檢查 Git 狀態
 git status
@@ -11,13 +9,21 @@ git status
 :: 新增全部修改
 git add .
 
-:: 自動建立 commit，包含當前時間
-for /f "tokens=1-4 delims=/ " %%a in ('date /t') do set mydate=%%a-%%b-%%c-%%d
-for /f "tokens=1-2 delims=: " %%a in ('time /t') do set mytime=%%a%%b
+:: 自動建立 commit，使用安全格式的時間戳
+setlocal enabledelayedexpansion
+for /f "tokens=1-3 delims=/- " %%a in ('date /t') do (
+    set mydate=%%a-%%b-%%c
+)
+for /f "tokens=1-2 delims=: " %%a in ('time /t') do (
+    set mytime=%%a%%b
+)
 git commit -m "Auto backup on %mydate%_%mytime%"
 
 :: 上傳到 GitHub
 git push
+
+:: （可選）上傳後自動開啟 GitHub 頁面
+start https://github.com/mandy15933/vsCode-backup
 
 echo.
 echo ✅ 專案已成功備份到 GitHub！
